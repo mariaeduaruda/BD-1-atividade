8a821b1b2332ce22b80b57b12d009b409d1d331e
mariaeduaruda <87451133+mariaeduaruda@users.noreply.github.com>
2021-12-10 23:04:12 -0300

    CORREÇÃO

--- BD1_2254077_Modelo_Relacional.sql
+++ BD1_2254077_Modelo_Relacional.sql
@@ -21,7 +21,7 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Consumidor (
-  ID_User INTEGER,
+  ID_User INTEGER PRIMARY KEY,
   name VARCHAR(100),
   endereco VARCHAR(100),
   cpf VARCHAR(15) NOT NULL,
@@ -30,6 +30,7 @@ CREATE TABLE Consumidor (
 
 CREATE TABLE Restaurante (
   ID_restaurante INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
   tipo_categoria  VARCHAR(50),
   horario_funcionamento VARCHAR(9),
   telefone VARCHAR(100),
@@ -37,7 +38,7 @@ CREATE TABLE Restaurante (
 ); 
 
 CREATE TABLE Proprietario (
-  ID_User INTEGER,
+  ID_User INTEGER PRIMARY KEY,
   ID_Restaurante  INTEGER,
   name VARCHAR(100),
   cnpj BIGINT UNIQUE NOT NULL,
@@ -47,7 +48,9 @@ CREATE TABLE Proprietario (
 CREATE TABLE Cartao (
   num_cartao INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nome_cartao VARCHAR(100),
-  validade VARCHAR(9)
+  validade VARCHAR(9),
+  ID_User INTEGER,
+  FOREIGN KEY (ID_User) REFERENCES Consumidor(ID_User)
 );
 
 CREATE TABLE Cupom (
@@ -59,10 +62,12 @@ CREATE TABLE Cupom (
 CREATE TABLE Pedido (
   ID_Pedido INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   ID_restaurante INTEGER,
+  ID_User  INTEGER,
   valor_total DECIMAL(20,2),
   hora_pedido VARCHAR(9),
   forma_pagamento  VARCHAR(15),
   endereco VARCHAR(100),
+  FOREIGN KEY (ID_User) REFERENCES Consumidor(ID_User),
   FOREIGN KEY (ID_restaurante) REFERENCES Restaurante(ID_restaurante)
 );
 
@@ -71,7 +76,7 @@ CREATE TABLE Avaliacao (
   ID_User  INTEGER,
   ID_Pedido  INTEGER,
   pontuacao DECIMAL,
-  FOREIGN KEY (ID_User) REFERENCES Users(ID_User),
+  FOREIGN KEY (ID_User) REFERENCES Consumidor(ID_User),
   FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido)
 );
 
@@ -89,6 +94,7 @@ CREATE TABLE Entregadores (
 );
 
 CREATE TABLE Entrega (
+  ID_entrega  INTEGER PRIMARY KEY,
   ID_entregador INTEGER,
   ID_pedido INTEGER,
   FOREIGN KEY (ID_entregador) REFERENCES Entregadores(ID_entregador),
@@ -116,8 +122,10 @@ CREATE TABLE Item (
 );
 
 CREATE TABLE Itens_Pedido (
+  ID_Itemp INTEGER PRIMARY KEY,
   ID_item INTEGER,
   ID_Pedido INTEGER,
   FOREIGN KEY (ID_item) REFERENCES Item(ID_item),
+  FOREIGN KEY (ID_restaurante) REFERENCES Restaurante(ID_restaurante),
   FOREIGN KEY (ID_Pedido) REFERENCES Pedido(ID_Pedido)
 );
