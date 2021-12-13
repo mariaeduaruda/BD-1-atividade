2803b647ce238775c504d9abac9161b30a8f78d3
mariaeduaruda <87451133+mariaeduaruda@users.noreply.github.com>
2021-12-12 21:01:53 -0300

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -16,7 +16,7 @@ VALUES
 (12,'amanda', 'BhBZrgb5C9'),
 (13,'freitas', '1dGh0I0R3b');
 
-INSERT INTO Consumidor(ID_User, nome, cpf, endereco)
+INSERT INTO Consumidor(ID_User, name, cpf, endereco)
 VALUES
 (2,'Antonio', 08527707071, 'Avenida Desembargador Moreira'),
 (4,'Cleber', 61657505081, 'Rua das Fiandeiras'),
@@ -27,7 +27,7 @@ VALUES
 (9,'Samir', 66849364081, 'QE 11 Área Especial C'),
 (10,'Sandro', 38293046090, 'Rua Arlindo Nogueira');
 
-INSERT INTO Restaurante(ID_Restaurante, nome, tipo_categoria, horario_funcionamento, telefone, endereco)
+INSERT INTO Restaurante(ID_Restaurante, name, tipo_categoria, horario_funcionamento, telefone, endereco)
 VALUES
 (1,'Mc dolnalds', 'lanche', '14 - 19h', '(95) 3130-4613', 'Avenida Coronel Manoel Inocêncio'),
 (2,'Dog King', 'lanche', '15 - 23h', '(94) 3500-4843', 'Via de Pedestre Serra do Taquari'),
@@ -40,7 +40,7 @@ VALUES
 (9,'Habibis', 'arabe', '12 - 15h', '(35) 3127-6506', 'Rua Luiz Pasteur'),
 (10,'Samurai', 'japones', '13 - 21h', '(79) 2012-1324', 'QE 11 Área Especial C');
 
-INSERT INTO Proprietario(ID_User, ID_Restaurante, nome, cnpj,)
+INSERT INTO Proprietario(ID_User, name, ID_Restaurante, cnpj)
 VALUES 
 (1,'Felipe', 1, 37698954000120),
 (3,'miguel', 2, 5900673033000145), 
@@ -50,10 +50,10 @@ VALUES
 
 INSERT INTO Cartao(num_cartao, nome_cartao, validade, ID_User)
 VALUES 
-(5138864788338633,'Felipe','27/11/2028', 4),
-(6363687530809625,'miguel','22/12/2030', 5);
-(4828724553368397,'Roberto','03/03/2025', 6),
-(6595377270855695,'Sandro','21/04/2029', 7);
+(88338633,'Felipe', '2028', 4),
+(30809625,'miguel','2030', 5),
+(3368397,'Roberto','2025', 6),
+(0855695,'Sandro','2029', 7);
 
 INSERT INTO Cupom(ID_Cupom, tipo_Cupom, porcentagem)
 VALUES 
@@ -62,20 +62,20 @@ VALUES
 
 INSERT INTO Pedido(ID_Pedido, ID_restaurante, ID_User, valor_total, hora_pedido, forma_pagamento, endereco)
 VALUES 
-(1, 1, 2, 20,90,'05/10/16 - 11:51', 'Cartão', 'Avenida Desembargador Moreira'),
-(2, 2, 2, 27,99,'09/11/20 - 13:20', 'Dinheiro' ,'Rua da Imprensa'),
-(3, 3, 4, 54,50,'03/12/17 - 18:57', 'Cartão', 'Rua das Fiandeiras'),
-(4, 4, 6, 20,90,'05/11/96 - 11:51', 'Cartão', 'Avenida Desembargador Moreira'),
-(5, 5, 7, 27,99,'09/10/20 - 14:20', 'Dinheiro' ,'Rua da Imprensa'),
-(6, 6, 8, 54,50,'03/08/17 - 10:57', 'Cartão', 'Rua das Fiandeiras'),
-(7, 7, 6, 20,90,'05/09/19 - 16:51', 'Cartão', 'Avenida Desembargador Moreira'),
-(8, 8, 9, 27,99,'09/01/20 - 12:20', 'Cartão' ,'Rua da Imprensa'),
-(9, 9, 10, 54,50,'03/09/17 - 10:57', 'Cartão', 'Rua das Fiandeiras'),
-(10, 10, 8, 54,50,'03/08/17 - 11:57', 'Cartão', 'Rua das Fiandeiras');
+(1, 1, 2, 20.9,'05/10/16 - 11:51', 'Cartão', 'Avenida Desembargador Moreira'),
+(2, 2, 2, 27.9,'09/11/20 - 13:20', 'Dinheiro' ,'Rua da Imprensa'),
+(3, 3, 4, 54.5,'03/12/17 - 18:57', 'Cartão', 'Rua das Fiandeiras'),
+(4, 4, 6, 20.9,'05/11/96 - 11:51', 'Cartão', 'Avenida Desembargador Moreira'),
+(5, 5, 7, 27.9,'09/10/20 - 14:20', 'Dinheiro' ,'Rua da Imprensa'),
+(6, 6, 8, 54.5,'03/08/17 - 10:57', 'Cartão', 'Rua das Fiandeiras'),
+(7, 7, 6, 20.9,'05/09/19 - 16:51', 'Cartão', 'Avenida Desembargador Moreira'),
+(8, 8, 9, 27.9,'09/01/20 - 12:20', 'Cartão' ,'Rua da Imprensa'),
+(9, 9, 10, 54.50,'03/09/17 - 10:57', 'Cartão', 'Rua das Fiandeiras'),
+(10, 10, 8, 54.50,'03/08/17 - 11:57', 'Cartão', 'Rua das Fiandeiras');
 
 INSERT INTO Statu(ID_status, ID_Pedido, progresso)
 VALUES
-(1,1,  'ASaiu para entrega'),
+(1,1,  'Saiu para entrega'),
 (2,2,'Em preparo'),
 (3,3,'Visto'),
 (4,4, 'Concluido'),
@@ -96,7 +96,7 @@ VALUES
 (6, 'Osvaldo Samuel Leandro da Luz', 37580580314),
 (7, 'Lucas Giovanni Assunção', 44925408810),
 (8, 'Theo Gael Enzo Freitas', 02815284707),
-(9 'Cauã Pedro Henrique Teixeira', 54166833685),
+(9, 'Cauã Pedro Henrique Teixeira', 54166833685),
 (10, 'Ryan Julio Henry Novaes', 72944554301 );
 
 INSERT INTO Entrega( ID_entrega, ID_entregador, ID_pedido )
@@ -114,18 +114,19 @@ VALUES
 
 INSERT INTO Categorias(ID_categoria, tipo_categoria, pontuacao)
 VALUES
-(1, 'Japones',  5.0),
-(2, 'caseiro', 4.6),
-(3, 'lanche', 4.3),
-(4, 'mercado', 4),
-(5, 'mexicano', 5),
-(6, 'arabe', 3.9),
-(7, 'sorvete', 3.7),
-(8, 'doce', 4.5),
-(9, 'fitness', 3.9),
-(10, 'pizza', 4.8);
+(1, 'Japones',  1),
+(2, 'caseiro', 2),
+(3, 'lanche', 3),
+(4, 'Japones',  5.0),
+(5, 'caseiro', 4.6),
+(6, 'lanche', 4.3),
+(7, 'mercado', 4.0)
+(8, 'mexicano', 5.0),
+(9, 'arabe', 4.6),
+(10, 'sorvete', 4.7),
+(11, 'pizza', 4.8);
 
-INSERT INTO Cardapio(ID_cardapio, ID_restaurante, itens_cardapio,)
+INSERT INTO Cardapio(ID_cardapio, ID_restaurante, itens_cardapio)
 VALUES
 (1, 1, 'Urumaki salmão, Hot filadelphia, Hot crispy couve, Sashimi tilapia, Seviche'),
 (2, 2, 'Aipim Frito, Polenta Com Frango, Caldo Verde'),
@@ -153,13 +154,13 @@ VALUES
 
 INSERT INTO Itens_Pedido(ID_Itemp, ID_item, ID_Pedido)
 VALUES
-(1, 1,  1),
-(2, 2, 2),
-(3, 3, 3),
-(4, 4, 4),
-(5, 5, 5),
-(6, 6, 6),
-(7, 7, 7),
-(8, 8, 8),
-(9, 9, 9),
-(10, 10, 10);
+(1, 1,  1, 10),
+(2, 2, 2, 9),
+(3, 3, 3, 8),
+(4, 4, 4, 1),
+(5, 5, 5, 2),
+(6, 6, 6, 4),
+(7, 7, 7, 7),
+(8, 8, 8, 5),
+(9, 9, 9, 6),
+(10, 10, 10, 3);
